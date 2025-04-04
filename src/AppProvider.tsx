80b48468751b76a247532ece4d357b17c38d2ddf
Quang.NguyenNhat <quangnguyennhat.axonactive@gmail.com>
2025-04-04 08:53:18 +0700

    test git on macOS

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,17 +1,17 @@
-import React, { createContext, useContext } from "react";
+// import React, { createContext, useContext } from "react";
 
-type AppContextType = {};
+// type AppContextType = {};
 
-const AppContext = createContext<AppContextType | undefined>(undefined);
+// const AppContext = createContext<AppContextType | undefined>(undefined);
 
-export const useAppContext = () => {
-  const context = useContext(AppContext);
-  if (!context) {
-    throw new Error("useAppContext must be used within a AppProvider");
-  }
-  return context;
-};
+// export const useAppContext = () => {
+//   const context = useContext(AppContext);
+//   if (!context) {
+//     throw new Error("useAppContext must be used within a AppProvider");
+//   }
+//   return context;
+// };
 
-export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
+// export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+//   return <>{children}</>;
+// };
